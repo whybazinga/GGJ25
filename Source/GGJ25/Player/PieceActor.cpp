133935857eb76ed64ba2fa1ed681490442a42450
Andrew Ivashkin <ghostsly1@gmail.com>
2025-01-25 11:50:59 +0100

    player piece actor

---
+++ Source/GGJ25/Player/PieceActor.cpp
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PieceActor.h"
+
+APieceActor::APieceActor()
+{
+    // Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    PrimaryActorTick.bCanEverTick = true;
+}
+
+void APieceActor::BeginPlay()
+{
+    Super::BeginPlay();
+    
+}
+
+void APieceActor::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+}
+
---
+++ Source/GGJ25/Player/PieceActor.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PieceActor.generated.h"
+
+UCLASS()
+class GGJ25_API APieceActor : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    APieceActor();
+
+protected:
+    virtual void BeginPlay() override;
+
+public:
+    virtual void Tick(float DeltaTime) override;
+};
